f07b05b6d422e344d2d8ce38425e48a7465b25d6
Mat <mvello66@gmail.com>
2021-03-23 21:11:04 -0400

    working get with axios, finally

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <hr>
+    <button v-on:click="indexProducts">click me</button>
+    <div v-if="toggleProducts">
+      {{ products }}
+    </div>
   </div>
 </template>
 <style></style>
 <script>
+import axios from "axios";
+
 export default {
-  data: function() {
+  data: function () {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      products: [],
+      toggleProducts: false,
     };
   },
-  created: function() {},
-  methods: {}
+  created: function () {},
+  methods: {
+    indexProducts: function () {
+      axios.get("http://localhost:3000/api/products").then((response) => {
+        console.log(response.data);
+      });
+    },
+  },
 };
 </script>
